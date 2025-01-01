6019559c4653640823a3df250eb8ac3bb11169b4
Sibusiso81 <113394852+Sibusiso81@users.noreply.github.com>
2025-01-01 19:24:22 +0000

    adding auth pages

---
+++ portfolio/app/Auth/Error/page.tsx
@@ -0,0 +1,17 @@
+import { Separator } from '@radix-ui/react-dropdown-menu'
+import { CircleX } from 'lucide-react'
+import React from 'react'
+
+function page() {
+  return (
+  <section className='w-screen h-screen flex flex-col items-center justify-center text-white dark:text-white  '>
+    <div className='flex flex-row space-x-3 mx-auto my-auto'>
+        <CircleX />
+        <Separator/>
+        <p>Sorry somthing went wrong</p>
+    </div>
+  </section>
+  )
+}
+
+export default page
