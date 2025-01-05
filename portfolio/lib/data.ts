cc7a316ecdbd552d11d3cc78686f27f50e6a3e9a
Sibusiso81 <113394852+Sibusiso81@users.noreply.github.com>
2025-01-05 13:40:48 +0000

    adding dashboard/Virtual Asistant....

---
+++ portfolio/lib/data.ts
@@ -0,0 +1,42 @@
+export async function getWeather() {
+    const { NEXT_WEATHER_API_KEY } = process.env;
+    const response =
+      await fetch(`http://api.weatherapi.com/v1/current.json?key=${NEXT_WEATHER_API_KEY}&q=Johannesburg
+      // 
+      //&aqi=no`);
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
+  }
+
+getWeather();
+
+export const getMotivationalQuote = async () => {
+  const url = 'https://api.api-ninjas.com/v1/quotes';
+  const NEXT_QUOTE_API_KEY = process.env.NEXT_QUOTE_API_KEY;
+
+  try {
+    if (!NEXT_QUOTE_API_KEY) {
+      throw new Error('NEXT_QUOTE_API_KEY is not defined');
+    }
+
+    const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+      method: 'GET',
+      headers: {
+        'X-Api-Key':'jfCf0OOz9rXCGk2KK/jreA==zsU8DERfjeMDNtSN'
+        ,
+        'Content-Type': 'application/json'
+      }
+    });
+
+
+    const quote = await response.json();
+  
+    console.log(quote[0].quote);
+    return quote;
+  } catch (error) {
+    console.log('Fetch error:', error);
+  }
+};
+
+getMotivationalQuote();
